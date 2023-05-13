a913f32f70904b913dee894e7b42752abba401fb
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-05-13 15:50:42 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../components/Navbar.jsx"
+import Main from "../components/Main.jsx"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
