ed99a74808b9dcab3a102d916af2f9aca6df8486
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-05-14 23:11:56 -0700

    refactor

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Navbar from "./components/Navbar.jsx"
-import Main from "./components/Main.jsx"
+import Navbar from "./Navbar"
+import Main from "./Main"
 
 export default function App() {
     return (
