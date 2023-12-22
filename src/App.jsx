72ad94a39ef9a8f821251e8da77673b7ddcc22b7
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-12-22 14:11:45 -0800

    Learned pulling data from file to add to cards. map()

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Card from './components/card'
+import data from './data'
 
 
 function App() {
-
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
   
   return (
     <div className="container">
       <Navbar />
-      <Hero />
-      <Card 
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
-      
+      <Hero /> 
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   )
 }
