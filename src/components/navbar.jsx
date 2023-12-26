f60b259caf0767128cbc339275af47d2b7bcd973
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2023-12-25 23:57:01 -0800

    Style Update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,17 @@ export default function Navbar() {
   return (
       <nav>
         <img className="abnb--logo" src="../public/assets/airbnb-logo.png" alt="logo" />
+        <section className="rightSide">
+          <div className="pillShape">
+            <span>Airbnb your home</span>
+          </div>
+          <img src="" alt="" />
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+
+        </section>
       </nav>
     )
 }
